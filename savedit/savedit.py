fcdc7ef99be9d5f21e574e617a09f2a199fcb194
Kevin Lane <lane.kevin.a@gmail.com>
2019-06-23 21:15:14 -0700

    Expand Savedit to store posts in a database

--- savedit/savedit.py
+++ savedit/savedit.py
@@ -2,9 +2,30 @@ from dropbox import Dropbox
 from pocket import Pocket
 from praw import Reddit
 from pushbullet import PushBullet
-from .config import *
 
-reddit = Reddit(user_agent='savedit v0.1.0 by /u/{}'.format(REDDIT_USERNAME))
+from __version__ import __version__
+from config import *
+from services import DatabaseService
+
+
+class Savedit(Reddit):
+    def __init__(self, site_name=None, requestor_class=None, requestor_kwargs=None, **config_settings):
+        super(Savedit, self).__init__(
+            site_name=site_name,
+            requestor_class=requestor_class,
+            requestor_kwargs=requestor_kwargs,
+            **config_settings
+        )
+
+        self.db = DatabaseService('../savedit.db')
+        self.db.reset()
+
+
+savedit = Savedit('savedit', user_agent='savedit v{} by /u/{}'.format(__version__, REDDIT_USERNAME))
+user = savedit.user.me()
+[savedit.db.save_post(post) for post in user.saved()]
+savedit.db.commit()
+savedit.db.close()
 
 pb = PushBullet(PUSHBULLET_TOKEN)
 
