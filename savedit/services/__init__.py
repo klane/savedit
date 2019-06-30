a62cc86c4c4d10ae098ba366f6cbcaa38afbcb84
Kevin Lane <lane.kevin.a@gmail.com>
2019-06-29 18:03:18 -0700

    Move Service class to package init

--- savedit/services/__init__.py
+++ savedit/services/__init__.py
@@ -1,2 +1,13 @@
-from .service import Service
+from abc import ABC, abstractmethod
+
 from .database import DatabaseService
+
+
+class Service(ABC):
+    @abstractmethod
+    def is_saved(self, post):
+        pass
+
+    @abstractmethod
+    def save_post(self, post):
+        pass
