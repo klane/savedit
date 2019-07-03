120512b96521bf64a35ca1d819346cfeb9067c9e
Kevin Lane <lane.kevin.a@gmail.com>
2019-07-02 21:26:15 -0700

    Add method to check if a notification is registered

--- savedit/notifications/__init__.py
+++ savedit/notifications/__init__.py
@@ -2,6 +2,11 @@ from abc import ABC, abstractmethod
 
 
 class Notification(ABC):
+    @staticmethod
+    @abstractmethod
+    def is_registered():
+        pass
+
     @abstractmethod
     def notify(self, post):
         pass
