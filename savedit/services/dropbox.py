d378da2d94ad63ae4d53864cfba5edb200b25862
Kevin Lane <lane.kevin.a@gmail.com>
2019-07-10 21:47:21 -0700

    Add Dropbox and Pocket service skeletons

--- savedit/services/dropbox.py
+++ savedit/services/dropbox.py
@@ -1,5 +1,23 @@
 from dropbox import Dropbox
 
 from savedit.config import DROPBOX_TOKEN
+from savedit.services import Service
 
-dropbox = Dropbox(DROPBOX_TOKEN)
+
+class DropboxService(Service):
+    def __init__(self):
+        self.dropbox = Dropbox(DROPBOX_TOKEN)
+
+    @staticmethod
+    def check_post(post):
+        pass
+
+    @staticmethod
+    def is_registered():
+        pass
+
+    def is_saved(self, post):
+        pass
+
+    def save_post(self, post):
+        pass
--- savedit/services/pocket.py
+++ savedit/services/pocket.py
@@ -1,5 +1,23 @@
 from pocket import Pocket
 
 from savedit.config import POCKET_KEY, POCKET_TOKEN
+from savedit.services import Service
 
-pocket = Pocket(POCKET_KEY, POCKET_TOKEN)
+
+class PocketService(Service):
+    def __init__(self):
+        self.pocket = Pocket(POCKET_KEY, POCKET_TOKEN)
+
+    @staticmethod
+    def check_post(post):
+        pass
+
+    @staticmethod
+    def is_registered():
+        pass
+
+    def is_saved(self, post):
+        pass
+
+    def save_post(self, post):
+        pass
