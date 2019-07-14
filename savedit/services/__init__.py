14a6afdc76e32bfe5e4826004414ac6bc568680e
Kevin Lane <lane.kevin.a@gmail.com>
2019-07-14 08:38:15 -0700

    Add database table for all services

--- savedit/services/__init__.py
+++ savedit/services/__init__.py
@@ -1,7 +1,14 @@
 from abc import ABC, abstractmethod
 
+from peewee import ForeignKeyField
+
+from ..database import BaseModel, Post
+
 
 class Service(ABC):
+    class Table(BaseModel):
+        post = ForeignKeyField(Post)
+
     @staticmethod
     @abstractmethod
     def check_post(post):
