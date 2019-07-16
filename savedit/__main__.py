3d71f2b20630d011930d86f292b04bebdddd0d76
Kevin Lane <lane.kevin.a@gmail.com>
2019-07-15 21:09:02 -0700

    Move script contents inside main function

---
+++ savedit/__main__.py
@@ -0,0 +1,4 @@
+from .savedit import main
+
+if __name__ == "__main__":
+    main()
--- savedit/savedit.py
+++ savedit/savedit.py
@@ -1,13 +1,15 @@
 from praw import Reddit
 
-from __version__ import __version__
-from config import *
-from database import DB, TABLES, Post
+from .__version__ import __version__
+from .config import *
+from .database import DB, TABLES, Post
 
-DB.drop_tables(TABLES)
-DB.create_tables(TABLES)
 
-reddit = Reddit('savedit', user_agent='savedit v{} by /u/{}'.format(__version__, REDDIT_USERNAME))
-user = reddit.user.me()
-[Post.create(post) for post in user.saved()]
-DB.close()
+def main():
+    DB.drop_tables(TABLES)
+    DB.create_tables(TABLES)
+
+    reddit = Reddit('savedit', user_agent='savedit v{} by /u/{}'.format(__version__, REDDIT_USERNAME))
+    user = reddit.user.me()
+    [Post.create(post) for post in user.saved()]
+    DB.close()
