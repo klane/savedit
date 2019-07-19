398f94450a53155f94b3ae5c46c130a9b77a6c2d
Kevin Lane <lane.kevin.a@gmail.com>
2019-07-19 11:28:42 -0700

    Add Pushbullet notification skeleton

--- savedit/notifications/pushbullet.py
+++ savedit/notifications/pushbullet.py
@@ -1,5 +1,16 @@
 from pushbullet import PushBullet
 
 from savedit.config import PUSHBULLET_TOKEN
+from savedit.integrations import Notification
 
-pb = PushBullet(PUSHBULLET_TOKEN)
+
+class PushbulletNotification(Notification):
+    def __init__(self):
+        self.pb = PushBullet(PUSHBULLET_TOKEN)
+
+    @staticmethod
+    def is_registered():
+        return True
+
+    def notify(self, post):
+        pass
